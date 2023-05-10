98517dcbc598a11e081cf98792f3c7489e1a3de4
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-05-10 17:22:38 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,33 +1,36 @@
-#include <iostream>
-#include <stack>
-
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stack>
 using namespace std;
 
 bool VPS(string str) {
-    stack<char> st; //문자열 입력받을 스택
-    for (int i = 0; i < str.length(); i++) { //문자열 끝까지
-        if (str[i] == '(') //"(" 일 때 push
-            st.push(str[i]); 
-        else if (str[i] == ')') {//")"일 때 
-            if (st.empty()) //비었는지 확인
-                 return false; //비어있다면 짝이 없으므로 false
-            else
-               st.pop();    //비어있지 않으면 pop
-        }
-    }
-    return (st.empty()); //반복문 후에 괄호가 있다면 짝 없음
-}
+	stack<char> s;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == '(') 
+			s.push(str[i]);
+		else if (str[i] == ')') {
+			if (s.empty()) 
 
+				return false; 
+
+			else
+				s.pop();
+		}
+		
+	}return (s.empty());
+}
 int main() {
-    int caseSize;
-    cin >> caseSize; //테스트 케이스 입력
-
-    for (int i = 0; i < caseSize; i++) {
-        string str;
-        cin >> str; //문자열 입력받기
-        if (VPS(str)) 
-            cout << "YES" << "\n";
-        else
-            cout << "NO" << "\n";
-    }
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		string str;
+		cin >> str;
+		if (VPS(str)) {
+			cout << "YES" << endl;
+		}
+		else
+			cout << "NO" << endl;
+	}
 }
+
