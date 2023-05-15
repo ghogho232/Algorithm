8bce57190807a732e593843ff48bc847154bcb38
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-05-15 20:10:08 +0900

    [Gold II] Title: Cow Frisbee, Time: 132 ms, Memory: 4892 KB -BaekjoonHub

--- 백준/Gold/24492. Cow Frisbee/Cow Frisbee.cc
+++ 백준/Gold/24492. Cow Frisbee/Cow Frisbee.cc
@@ -2,27 +2,31 @@
 #include<algorithm>
 #include<stack>
 #include<vector>
-#include <time.h>
-typedef long long ll;
+
 using namespace std;
 
-
 int main() {
-	    int N; cin >> N;
-    vector<ll> V(N); for(auto &v : V) cin >> v;
-    
-    // set<int> st;
-    stack<ll> st;
-    ll ans = 0;
-    
-    for(int i = 0; i < N; i++){
-        while(!st.empty() && V[st.top()] < V[i]){
-            ans += i - st.top() + 1;
-            st.pop();
-        }
-        if(!st.empty()) ans += i - st.top() + 1;
-        st.push(i);
-    } 
-    cout << ans << endl;
-    return 0;
+	int n;
+	cin >> n;
+	vector<long long> t(n);
+	for (int i = 0; i < n; i++) {
+		cin >> t[i];
+	}
+
+	stack<long long> idx;
+	long long ans = 0;
+
+	for (int i = 0; i < n; i++) {
+		while (!idx.empty() && t[idx.top()] < t[i]) {
+			ans += i - idx.top() + 1;
+			idx.pop();
+		}
+		if (!idx.empty()) {
+			ans += i - idx.top() + 1;
+		}
+		idx.push(i);
+	}
+
+	cout << ans << "\n";
+	return 0;
 }
