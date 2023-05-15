91594d091f7f2498ae88bcd07e0498c62b288847
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-05-15 20:00:01 +0900

    [Gold II] Title: Cow Frisbee, Time: 132 ms, Memory: 4896 KB -BaekjoonHub

--- 백준/Gold/24492. Cow Frisbee/Cow Frisbee.cc
+++ 백준/Gold/24492. Cow Frisbee/Cow Frisbee.cc
@@ -1,53 +1,28 @@
-#include <bits/stdc++.h>
-
+#include<iostream>
+#include<algorithm>
+#include<stack>
+#include<vector>
+#include <time.h>
+typedef long long ll;
 using namespace std;
-using ll = long long;
-using pl = pair<ll, ll>;
-using vl = vector<ll>;
 
-#define forn(i, n) for(ll i = 0; i < n; i++)
-#define rofn(i, n) for(ll i = n - 1; i >= 0; i--)
-#define f first
-#define s second
 
-int main(){
-	ll n; cin >> n;
-	vl h(n), l(n), r(n);
-	stack<pl> st1, st2;
-	forn(i, n){
-		cin >> h[i];
-		l[i] = i, r[i] = i;
-	}
-	forn(i, n){
-		while(!st1.empty()){
-			if(st1.top().f > h[i]){
-				l[i] = st1.top().s;
-				break;
-			} else {
-				st1.pop();
-			}
-		}
-		st1.push({h[i], i});
-	}
-	rofn(i, n){
-		while(!st2.empty()){
-			if(st2.top().f > h[i]){
-				r[i] = st2.top().s;
-				break;
-			} else {
-				st2.pop();
-			}
-		}
-		st2.push({h[i], i});
-	}
-	ll res = 0;
-	forn(i, n){
-		if(i != l[i]){
-			res += (i - l[i] + 1);
-		}
-		if(i != r[i]){
-			res += (r[i] - i + 1);
-		}
-	}
-	cout << res << endl;
+int main() {
+	    int N; cin >> N;
+    vector<ll> V(N); for(auto &v : V) cin >> v;
+    
+    // set<int> st;
+    stack<ll> st;
+    ll ans = 0;
+    
+    for(int i = 0; i < N; i++){
+        while(!st.empty() && V[st.top()] < V[i]){
+            ans += i - st.top() + 1;
+            st.pop();
+        }
+        if(!st.empty()) ans += i - st.top() + 1;
+        st.push(i);
+    } 
+    cout << ans << endl;
+    return 0;
 }
