3124107860ac8d820b94cbe52922f7ccbc0c836d
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-08 20:11:53 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+	while (1) {
+		int b, c;
+		cin >> b >> c;
+		if (cin.eof()) {
+			break;
+		}
+		cout << b + c << "\n";
+	}
+	return 0;
+}
