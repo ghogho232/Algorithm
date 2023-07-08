c639f61c55975e83a8f2488cf67cef6f5652d3d1
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-08 18:14:39 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	 double a, b;
+
+    cin >> a >> b;
+
+    cout.precision(15);
+    cout << a / b;
+
+    return 0;
+}
