0e20dadee0f3a7eb6285b3c3146c230295218be6
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-11 18:27:36 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<math.h>
+#include<queue>
+#include<vector>
+#include<stack>
+
+
+using namespace std;
+
+int main() {
+	string str;
+	while (getline(cin, str)) {
+		cout << str << "\n";
+	}
+	return 0;
+}
