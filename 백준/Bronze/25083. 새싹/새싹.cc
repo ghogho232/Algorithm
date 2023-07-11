5192ad5e9b81f9d019d286a2277422e3239ed69b
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-11 18:28:37 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<math.h>
+#include<queue>
+#include<vector>
+#include<stack>
+
+
+using namespace std;
+
+int main() {
+    cout << "         ,r'\"7 \n";
+    cout << "r`-_   ,'  ,/ \n";
+    cout << " \\. \". L_r' \n";
+    cout << "   `~\\/ \n";
+    cout << "      | \n";
+    cout << "      | \n";
+
+    return 0;
+}
