6b9010dc41d4772f6c4ca49ee6538dff59c65fc0
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-11 16:46:50 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<queue>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+int main() {
+	string str;
+	int n;
+	cin >> str;
+	cin >> n;
+	cout << str[n-1];
+}
