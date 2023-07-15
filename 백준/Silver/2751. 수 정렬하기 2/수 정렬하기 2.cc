4e96c7be9ad28a80a0a640ad132abb39b8ff931c
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-15 11:18:47 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 684 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<math.h>
+#include<queue>
+#include<vector>
+#include<stack>
+
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	long long *arr;
+	arr = new long long[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << "\n";
+	}
+	delete arr;
+	return 0;
+}
