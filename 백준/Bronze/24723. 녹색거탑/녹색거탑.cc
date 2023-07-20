c768ea88bdaa62356ace8950741fb612faecfca5
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-20 22:30:44 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<math.h>
+#include<queue>
+#include<vector>
+#include<stack>
+#include<map>
+#include<set>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << pow(2,n);
+    return 0;
+}
