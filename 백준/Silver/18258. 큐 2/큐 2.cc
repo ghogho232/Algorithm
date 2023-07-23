0037e6696f2e8f8e5bcf9a6786687cad52257823
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-23 16:48:24 +0900

    [Silver IV] Title: 큐 2, Time: 400 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<math.h>
+#include<queue>
+#include<vector>
+#include<stack>
+#include<map>
+#include<set>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    queue<int>q;
+    int n;
+    cin >> n;
+    while (n--) {
+        string str;
+        cin >> str;
+        if (str == "push") {
+            int a;
+            cin >> a;
+            q.push(a);
+        }
+        else if (str == "pop"){
+            if (!q.size())
+                cout << -1 << "\n";
+            else {
+                cout << q.front()<<"\n";
+                q.pop();
+            }
+        }
+        else if (str == "size") {
+            cout << q.size()<<"\n";
+        }
+        else if (str == "empty") {
+            if (q.empty())
+                cout << 1 << "\n";
+            else
+                cout << 0 << "\n";
+        }
+        else if (str == "front") {
+            if (q.empty())
+                cout << -1 << "\n";
+            else
+                cout << q.front() << "\n";
+        }
+        else if (str == "back") {
+            if (q.empty())
+                cout << -1 << "\n";
+            else
+                cout << q.back() << "\n";
+        }
+    }
+    return 0;
+}
