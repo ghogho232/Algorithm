d241c4432619c6be32aa55ce74dfef29eed98640
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-27 19:07:29 +0900

    [Silver III] Title: N과 M (3), Time: 352 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<math.h>
+#include<queue>
+#include<vector>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include <sstream>
+
+using namespace std;
+
+const int MAX = 9;
+
+int n, m;
+int arr[MAX] = { 0 };
+int visited[MAX] = { 0 };
+
+void dfs(int v) {
+	if (v == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		
+			visited[i] = 1;
+			arr[v] = i;
+			dfs(v + 1);
+			visited[i] = 0;
+		
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	cin >> n >> m;
+	dfs(0);
+
+	return 0;
+}
