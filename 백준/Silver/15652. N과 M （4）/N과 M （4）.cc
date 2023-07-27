63384c50683c137d49c2aa788ad0d71cf4201435
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-28 01:19:20 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -18,27 +18,27 @@ const int MAX = 9;
 
 int n, m;
 int arr[MAX] = { 0 };
+int visited[MAX] = { 0 };
 
-void dfs(int v, int idx) {
-    if (v == m) {
-        for (int i = 0; i < m; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << "\n";
+void dfs(int v,int idx) {
+	if (v == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
         return;
-    }
-    for (int i = idx; i <= n; i++) {
-        arr[v] = i;
-        dfs(v + 1, i); 
-    }
+	}
+	for (int i = idx; i <= n; i++) {
+		arr[v] = i;
+		dfs(v + 1, i); 
+	}
 }
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> n >> m;
-    dfs(0, 1);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	cin >> n >> m;
+	dfs(0,1);
 
-    return 0;
+	return 0;
 }
