abbffd1e5900a78c9fb31c2451188dd6ec302c8a
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-28 00:20:17 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -21,27 +21,25 @@ int arr[MAX] = { 0 };
 int visited[MAX] = { 0 };
 
 void dfs(int v) {
-	if (v == m) {
-		for (int i = 0; i < m; i++) {
-			cout << arr[i] << " ";
-		}
-		cout << "\n";
-	}
-	for (int i = 1; i <= n; i++) {
-		if (!visited[i]){
-			visited[i] = 1;
-			arr[v] = i;
-			dfs(v + 1);
-			visited[i] = 0;
-		}
-	}
+    if (v == m) {
+        for (int i = 0; i < m; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            visited[i] = 1;
+            arr[v] = i;
+            dfs(v + 1);
+            visited[i] = 0;
+        }
+    }
 }
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); 
-	cout.tie(NULL);
-	cin >> n >> m;
-	dfs(0);
-
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    dfs(0);
 }
