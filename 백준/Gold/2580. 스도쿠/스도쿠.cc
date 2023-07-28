1768bf95e3579c15cfa6a57a902da8281866dd3f
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-29 01:00:58 +0900

    Update 스도쿠.cc

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -17,44 +17,47 @@ using namespace std;
 const int MAX = 10;
 
 int n;
-int s[MAX][MAX];
-vector<pair<int, int>>v;
+int s[MAX][MAX]; //스도쿠 입력받을 배열
+vector<pair<int, int>>v; //빈칸을 입력할 벡터
 
-bool check(int y,int x, int i) {
-    for (int j = 0; j < 9; j++) {
-        if (s[y][j] == i || s[j][x] == i) {
-            return false;
+//좌표에 i라는 수가 대입될 때 그게 유효한지 판단하는 함수
+bool check(int y,int x, int i) { 
+    for (int j = 0; j < 9; j++) { //동일한 라인을 돌면서
+        if (s[y][j] == i || s[j][x] == i) { //같은 수가 존재한다면
+            return false; //false
         }
     }
-    for (int j = (y / 3) * 3; j < (y / 3) * 3 + 3; j++) {
+    for (int j = (y / 3) * 3; j < (y / 3) * 3 + 3; j++) { //3*3사각형 내에
         for (int k = (x / 3) * 3; k < (x / 3) * 3 + 3; k++) {
-            if (s[j][k] == i)
-                return false;
+            if (s[j][k] == i) //같은 수가 존재한다면
+                return false; //flase
         }
     }
-    return true;
+    return true; //위에 해당이 없다면 true
 }
+
+//현재 빈 좌표에 대해 백트래킹
 bool dfs(int cur) {
-    if (cur == v.size()) {
+    if (cur == v.size()) { //모든 좌표가 채워졌다면
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                cout << s[i][j] << " ";
+                cout << s[i][j] << " "; //스도쿠 출력
             }
             cout << "\n";
         }
-        return true;
+        return true; //true반환(스도쿠완성)
     }
     int x = v[cur].second;
     int y = v[cur].first;
-    for (int i = 1; i <= 9; i++) {
-        if (check(y, x, i)) {
-            s[y][x] = i;
-            if(dfs(cur + 1))
-                return true;
-            s[y][x] = 0;
+    for (int i = 1; i <= 9; i++) { //1부터 9까지 빈칸에 넣어보며
+        if (check(y, x, i)) { //유효한지 확인
+            s[y][x] = i; //유효하다면 빈칸에 i삽입
+            if(dfs(cur + 1)) //다음 빈칸 재귀 처리 true라면
+                return true; //dfs도 true반환
+            s[y][x] = 0; //현재 i가 답이 아니라면 초기화 후 백트래킹
         }
     }
-    return false;
+    return false; //false반환으로 백트래킹
 }
 int main() {
     ios_base::sync_with_stdio(false);
