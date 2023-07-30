679e848c6c6123b33d4d75c765ec6ce79dfb429d
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-30 13:02:04 +0900

    Update 스타트와 링크.cc

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -14,56 +14,47 @@
 
 using namespace std;
 
-int stats[21][21];
-bool check[22];
-int N;
-int ans = 1000000000; // 10억
+int n; // 사람 수
+int arr[21][21]; // 능력치 배열
+int check[21]; // 뽑혔는지 확인 할 배열
 
-void DFS(int x, int pos) // x는 카운트 수, pos는 다음 값
-{
-	if (x == N / 2) // 카운트수가 정원의 1/2이 됐을 때 능력치합 계산
-	{
-		int start, link;
-		start = 0;
-		link = 0;
+int dfs(int v, int next) { // next로 이미 중복 방지
+    if (v == n / 2) { // 반이 뽑혔으면 다른 반은 자동으로 완성
+        int star = 0, link = 0; // 각 팀의 초기 능력치값
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (check[i] && check[j]) // 뽑힌 사람
+                    star += arr[i][j]; // 능력치 합
+                if (!check[i] && !check[j])
+                    link += arr[i][j];
+            }
+        }
+        int tmp = abs(star - link); // 두 팀의 능력치 차
+        return tmp; // 최소값 반환
+    }
 
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 1; j <= N; j++)
-			{
-				if (check[i] == true && check[j] == true) start += stats[i][j];
-				if (check[i] == false && check[j] == false) link += stats[i][j];
-			}
-		}
+    int minDiff = 1000000000;
+    for (int i = next; i <= n; i++) { // 중복없이
+        check[i] = 1; // i번째 사람 체크
+        int diff = dfs(v + 1, i + 1); // 그 뒤로 계속 체크
+        check[i] = 0; // 백트래킹
+        minDiff = min(minDiff, diff); // 최소값 갱신
+    }
 
-		int temp = abs(start - link);
-		if (ans > temp) ans = temp;
-
-		return;
-	}
-
-	for (int i = pos; i < N; i++)
-	{
-		check[i] = true;
-		DFS(x + 1, i + 1);
-		check[i] = false;
-	}
-
+    return minDiff; // 최소값 반환
 }
 
-int main()
-{
-	cin >> N;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			cin >> stats[i][j];
-		}
-	}
-
-	DFS(0, 1); // 카운트 0회부터 숫자는 1부터 시작
-
-	cout << ans;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    int MIN = dfs(0, 1); // 최소값 계산
+    cout << MIN;
 }
