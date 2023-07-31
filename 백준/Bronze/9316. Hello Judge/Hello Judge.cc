c150f23f6ba5c66bfd4d2a98fb7a6d6dfdf36325
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-07-31 19:41:44 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<math.h>
+#include<queue>
+#include<vector>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 1;i<=n;i++){
+        cout << "Hello World, Judge " <<i<<"!\n";
+    }
+    return 0;
+}
