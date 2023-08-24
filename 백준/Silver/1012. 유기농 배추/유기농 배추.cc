08c24bc4d3b36d3c3a5a3df5b600c535d98be59f
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-08-24 19:39:10 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,57 +1,69 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<cstdio>
 #include<iostream>
-#include<string>
+#include<algorithm>
+#include<string.h>
 #include<math.h>
 #include<queue>
+#include<vector>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include <sstream>
+
 using namespace std;
 
-const int MAX = 51;
-int map[MAX][MAX] = { 0 };
+int graph[51][51] = { 0 };
+int visit[51][51] = { 0 };
+int dx[4] = { 0, -1, 0, 1 };
+int dy[4] = { -1, 0, 1, 0 };
+int n, m, k;
+int cnt = 0;
 
-int m, n, k;
+void dfs(int x, int y) {
+    visit[x][y] = 1;
+    int nx, ny;
 
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            nx = x + dx[i];
+            ny = y + dy[i];
 
-void dfs(int y, int x) {
-
-	if (y < 0 || y >= n || x < 0 || x >= m || !map[y][x])
-		return;
-
-	map[y][x] = 0;
-	dfs(y + 1,x);
-	dfs(y,x+1);
-	dfs(y - 1,x);
-	dfs(y,x - 1);
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m || visit[nx][ny] || !graph[nx][ny])
+                continue;
+            else
+                dfs(nx, ny);
+        }
+    }
 }
-
-void process(int testcase) {
-	cin >> m >> n >> k;
-	int cnt = 0;
-	for (int i = 0; i < k; i++) {
-
-		int y,x;
-		cin >> y >> x;
-		map[x][y] = 1;
-	}
-
-	for (int y = 0; y < n; y++) {
-		for (int x = 0; x < m; x++) {
-			if (map[y][x]) {
-				dfs(y, x);
-				cnt++;
-			}
-		}
-	}
-
-	cout << cnt<<"\n";
-}
-
 int main() {
-	int testcase;
-	cin >> testcase;
-	for (int i = 0; i < testcase; i++) {
-		process(i);
-	}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL); 
+    int t;
+    cin >> t;
+    while (t--) {
+        cnt = 0;
+        memset(graph, 0, sizeof(graph));
+        memset(visit, 0, sizeof(visit));
+        cin >> n >> m >> k;
+        for (int i = 0; i < k; i++) {
+            int a, b;
+            cin >> a >> b;
+            graph[a][b] = 1;
+        }
 
-	return 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visit[i][j] && graph[i][j]) {
+                    dfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+        cout << cnt << "\n";
+    }
+    return 0;
+
 }
