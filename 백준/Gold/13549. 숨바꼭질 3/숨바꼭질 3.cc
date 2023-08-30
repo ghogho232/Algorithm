f5abf41bc9db89e46d779df8ec2e6a4f33d961ad
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-08-30 01:31:33 -0700

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -15,45 +15,44 @@
 #define ll long long
 
 using namespace std;    
-
 const int MAX = 100001;
-int visited[MAX] = { 0 };
-queue<pair<int, ll>>q;
-int n, k;
+queue<pair<int,ll>>q;
+int visited[MAX] = {0};
+int n,k;
 
-int bfs(int v) {
-    q.push({ v,0 });
+int bfs(int v){
     visited[v] = 1;
-
-    while (!q.empty()) {
+    q.push({v,0});
+    while(!q.empty()){
         int x = q.front().first;
         ll cnt = q.front().second;
         q.pop();
 
-        if (x * 2 < MAX && !visited[2 * x]) {
-            q.push({ 2 * x, cnt});
-            visited[x * 2] = 1;
+        if(2*x < MAX && !visited[2*x]){
+            q.push({2*x,cnt});
+            visited[2*x] = 1;
         }
-        if (x - 1 >= 0 && !visited[x - 1]) {
-            q.push({ x - 1,cnt + 1 });
-            visited[x - 1] = 1;
+        if(x-1>=0 && !visited[x-1]){
+            q.push({x-1,cnt+1});
+            visited[x-1] = 1;
+        }        
+        if(x+1 < MAX &&!visited[x+1]){
+            q.push({x+1,cnt+1});
+            visited[x+1] = 1;
         }
-        if (x + 1 < MAX && !visited[x + 1]) {
-            q.push({ x + 1, cnt + 1 });
-            visited[x + 1] = 1;
-        }
-        if (x == k)
+
+        if(x == k)
             return cnt;
     }
     return 0;
 }
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
     cin >> n >> k;
+    
     cout << bfs(n);
-	return 0;
-
+    return 0;
 }
