e6f3cdee0d3efd09ded4f6fd603c2fddce1406f7
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-09-03 19:27:31 +0900

    [Silver III] Title: 두 수의 합, Time: 12 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,38 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
 #include<iostream>
 #include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<queue>
 #include<vector>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include <sstream>
+#include <iomanip>
+
 using namespace std;
 
-int main() {
-	int n, x;
-	vector<int>v;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int a;
-		cin >> a;
-		v.push_back(a);
-	}
-	cin >> x;
-	sort(v.begin(), v.end());
-	int left = 0;
-	int right = n - 1;
-	int cnt = 0;
-	while (1) {
-		if (left >= right)
-			break;
-		int sum = v[left] + v[right];
-		if (sum == x) {
-			cnt++;
-			left++;
-			right--;
-		}
-		else if (sum < x) {
-			left++;
-		}
-		else
-			right--;
-	}
-	cout << cnt;
-	return 0;
-}
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n, x, cnt = 0;
+    int arr[100001];
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cin >> x;
+    sort(arr,arr+n);
+    int start = 0, end = n-1;
 
+
+    while(start < end){
+        int sum = arr[start] + arr[end];
+        if (sum == x) {
+            cnt++;
+            start++; 
+        } else if (sum > x) {
+            end--;
+        } else {
+            start++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
