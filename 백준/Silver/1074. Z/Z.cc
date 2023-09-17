e8ad93553a26df6da09b5f1d7c1262c9c7340636
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-09-17 21:31:01 +0900

    [Silver I] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<math.h>
+#include<queue>
+#include<vector>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include <sstream>
+#include <iomanip>
+#define ll long long
+
+using namespace std;
+
+int solve(int n, int r, int c) {
+	if (n == 0) 
+        return 0;
+	int half = 1 << (n - 1); 
+	if (r < half && c < half) 
+        return solve(n - 1, r, c);
+	if (r < half && c >= half) 
+        return (half * half) + solve(n - 1, r, c - half);
+	if (r >= half && c < half) 
+        return 2 * (half * half) + solve(n - 1, r - half, c);
+	return 3 * (half * half) + solve(n - 1, r - half, c - half);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, r, c;
+    cin >> n >> r >> c;
+    cout << solve(n,r,c);
+
+    return 0;
+}
