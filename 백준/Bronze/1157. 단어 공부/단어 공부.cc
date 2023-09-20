f367d7ab6c311865050a453a0571902857fca5a7
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-09-20 20:17:21 +0900

    [Bronze I] Title: 단어 공부, Time: 12 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.cc
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -7,36 +7,47 @@
 #include<queue>
 #include<vector>
 #include<stack>
-
+#include<map>
+#include<set>
+#include<deque>
+#include <sstream>
+#include <iomanip>
+#define ll long long
 
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    string str;
+    int arr[27] = {0};
+    int m = 0;
+    string ans;
+    int check = 0;
+    cin >> str;
+    for(int i = 0; i < str.length(); i++){
+        if(str[i] >= 'a' && str[i] <= 'z')
+        str[i] = str[i] - 'a' + 'A';
+        arr[str[i] - 'A']++;
+    }
 
-	int arr[26] = { 0 };
-	int max = -1; 
-	char res;
-	string str;
+    for(int i = 0; i <= 26; i++){
+        if(m < arr[i]){
+        m = arr[i];
+        ans = 'A' + i;            
+        }
 
-	cin >> str;
+    }
+    for(int i = 0; i <= 26; i++){
 
-	for (int i = 0; i < str.length(); i++) {
-		if (str[i] >= 'a' && str[i] <= 'z') str[i] = str[i] - 'a' + 'A'; 
+        if(m == arr[i]) 
+            check++;
+    }
+    if(check == 1)
+        cout << ans <<"\n";        
+    else    
+        cout << "?";
 
-		if (++arr[str[i] - 'A'] > max) {
-			max = arr[str[i] - 'A'];
-			res = str[i];
-		}
-	}
-
-	int check = 0; 
-
-	for (int i = 0; i < 26; i++) {
-		if (max == arr[i]) check++;
-	}
-
-	if (check == 1) cout << res;
-	else cout << "?";
-
-	return 0;
+    return 0;
 }
