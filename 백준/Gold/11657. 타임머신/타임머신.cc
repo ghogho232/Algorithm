8c4395c262ca0fc791739dcfd2b26e6515e1c7c4
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-09-27 13:25:03 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -12,57 +12,47 @@
 #include<deque>
 #include <sstream>
 #include <iomanip>
-#define MAX 501
+#define MAX 501 
 #define INF 1e9
 using namespace std;
-vector<pair<int, int>> graph[MAX];
+vector<pair<int,int>> adj[501];
 vector<long long> dist;
 int N, M;
-
-int main(void) {
+int main(void){
     ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(0); cout.tie(0);
 
-    cin >> N >> M;
-    // 입력된 간선 정보를 그래프에 저장
-    for (int i = 0; i < M; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        graph[a].push_back({c, b});
+    cin >> N >> M ;
+    for(int i=0; i<M; i++){
+        int src, dest, cost;
+        cin >> src >> dest >> cost;
+        adj[src].push_back({cost, dest});
     }
 
-    // 거리 배열 초기화
-    dist.resize(N + 1, INF);
-    dist[1] = 0;
+    dist.resize(N+1, INF);
+    dist[1] = 0 ;
 
     bool Cycle = false;
-    // 최단 거리 갱신
-    for (int round = 1; round <= N; round++) {
-        for (int i = 1; i <= N; i++) {
-            for (auto item : graph[i]) {
+    for( int round = 1;  round <= N ; round ++ ){
+        // 모든 edge 들을 조사한다.
+        for(int i=1; i<=N; i++){
+            for(auto item : adj[i]){
                 int cost = item.first;
                 int next = item.second;
 
-                // 현재 거리와 비교하여 최단 거리 갱신
-                if (dist[i] != INF && dist[next] > dist[i] + cost) {
+                if( dist[i] != INF && dist[next] > dist[i] + cost ){
                     dist[next] = dist[i] + cost;
-                    if (round == N) // 음수 사이클 검출
-                        Cycle = true;
+                    if( round == N ) Cycle = true;
                 }
             }
         }
     }
-    // 음수 사이클이 있을 경우 출력
-    if (Cycle)
+        if(Cycle)
         cout << -1 << '\n';
-    else {
-        // 1번 정점을 제외한 나머지 정점의 최단 거리 출력
-        for (int i = 2; i <= N; i++) {
-            if (dist[i] >= INF)
-                cout << -1 << '\n';
-            else
-                cout << dist[i] << '\n';
+    else{
+        for(int i=2; i<=N; i++){
+            if( dist[i] >= INF ) cout << -1 << '\n';
+            else cout << dist[i] << '\n';
         }
     }
 
