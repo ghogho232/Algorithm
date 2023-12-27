c4cc653890d443c07bbb98d46183c6f9921d608f
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2023-12-27 20:50:33 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -11,25 +11,27 @@
 #include<set>
 #include<deque>
 #include <sstream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int n;
-    cin >> n;
-    vector<int>v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    sort(v.begin(), v.end());
-    int sum = v[0];
-    for (int i = 1; i < n; i++) {
-        v[i] += v[i - 1];
-        sum += v[i];
-    }
-    cout << sum;
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	int sum = 0, ans = 0;
+	cin >> n;
+	vector<int>v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < n; i++) {
+		sum += v[i];
+		ans += sum;
+	}
+	cout << ans;
+
+	return 0;
 }
