289b6a280002091336204a2e3faccde557ad68d9
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2024-01-07 23:01:20 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 12 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -16,22 +16,23 @@
 
 using namespace std;
 
+int n;
+vector<ll>dp(1000001);
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
     cin >> n;
-    while (n--) {
-        ll dp[1000001];
+    dp[1] = 1, dp[2] = 2, dp[3] = 4, dp[4] = 7;
+    for (int i = 5; i < 1000001; i++) {
+        dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1]) % 1000000009;
+    }
+    for(int i = 0; i < n; i++) {  
         int a;
         cin >> a;
-        dp[1] = 1, dp[2] = 2, dp[3] = 4, dp[4] = 7;
-        for (int i = 5; i <= a; i++) {
-            dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1])%1000000009;
-        }
-        cout << dp[a] % 1000000009 <<"\n";
+        cout << dp[a] <<"\n";
     }
     return 0;
 }
