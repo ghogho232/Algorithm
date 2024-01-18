17681062e51099a8f74dfaf1f38bdffa27c8a781
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2024-01-18 19:27:56 +0900

    [Gold IV] Title: 테트로미노, Time: 80 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -68,7 +68,9 @@ void dfs(int y, int x, int depth, int sum){
 }
 
 int main() {
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> n >> m;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
