a3c0d2b3a6f7b9d357a18f37b8d51de27097d4e4
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2024-07-02 22:34:59 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,35 +1,55 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_nO_WARnInGS
 #include<cstdio>
 #include<iostream>
+#include<algorithm>
 #include<string>
+#include<cstring>
+#include<string.h>
 #include<math.h>
 #include<queue>
+#include<vector>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include <sstream>
+#include <iomanip>
+#define ll long long
+#define INF 987654321
+
 using namespace std;
-
-int map[200][200] = { 0 };
-int visited[200] = { 0 };
+int n,m;
+int arr[101][101] = {0};
+int visited[101] = {0};
 int cnt = 0;
-void dfs(int v) {
-	visited[v] = 0;
-	//cout << v << " ";
-	for (int i = 0; i < 200; i++) {
-		if (map[v][i] && visited[i]) {
-			cnt++;
-			dfs(i);
-		}
-	}
+
+void bfs(int v){
+    queue<int>q;
+    q.push(v);
+    visited[v] = 1;
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+        for(int i = 1; i <= n; i++){
+            if(arr[cur][i] == 1 && visited[i] == 0){
+                q.push(i);
+                visited[i] = 1;
+                cnt++;
+            }
+        }
+    }
+}
+int main() {
+    cin >> n >> m;
+    
+    for(int i = 0; i < m; i++){
+        int a,b;
+        cin >> a >> b;
+        arr[a][b] = arr[b][a] = 1;
+    }
+    bfs(1);
+    cout << cnt;
+
+    return 0;
 }
 
-int main(void) {
-	int node, edge;
-	cin >> node;
-	cin >> edge;
-	int v1, v2;
-	for (int i = 0; i < edge; i++) {
-		cin >> v1 >> v2;
-		map[v1][v2] = map[v2][v1] = 1;
-		visited[v1] = visited[v2] = 1;
-	}
-	dfs(1);
-	cout << cnt;
-}
