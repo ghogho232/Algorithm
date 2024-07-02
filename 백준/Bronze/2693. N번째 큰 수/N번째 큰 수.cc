fe3080033c347233d1923474c0e8e8d75f242be8
ghogho232 <130382538+ghogho232@users.noreply.github.com>
2024-07-02 22:55:20 +0900

    [Bronze I] Title: N번째 큰 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.cc
@@ -22,9 +22,12 @@ using namespace std;
 
 
 int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(0);        
+    cout.tie(0);
     int t;
     cin >> t;
-    
+
     while(t--){
         int arr[10];
         for(int i=0;i<10;i++){
@@ -36,4 +39,3 @@ int main() {
 
     return 0;
 }
-
